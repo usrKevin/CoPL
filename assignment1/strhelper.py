b9993c4ca3d02618ad9b3b34ef3083c52d8fdf1e
Florian <s3612759@vuw.leidenuniv.nl>
2023-11-24 00:41:29 +0100

    Some further changes to the recursion method also last commit before bed

--- assignment1/strhelper.py
+++ assignment1/strhelper.py
@@ -4,48 +4,45 @@ ALPHA = "abcdefghijklmnopqrstuvwxyz"
 NUM = "0123456789"
 ALLCHAR = ALPHA + NUM
 
-def do_err(reason:str = "Something went wrong!", index:int = -1):
+def do_err(reason:str = "Something went wrong", index:int = -1):
     print("ERROR: " + reason + (f" | at char: {index}" if index >= 0 else ""), file=sys.stderr)
     exit(1)
 
 
 def check_var(s:str,index:int = -1):
-    if len(s) == 0: do_err("Empty var!",index)
-    if s[0] not in ALPHA: do_err("Var must start with a letter!",index)
+    if len(s) == 0: do_err("Empty var",index)
+    if s[0] not in ALPHA: do_err("Var must start with a letter",index)
     for c in s:
-        if c not in ALLCHAR: do_err("Illegal var name!",index)
-
-class Var:
-    def __init__(self, index:int, content:str):
-        self.index = index
-        self.content = content
+        if c not in ALLCHAR: do_err("Illegal var name",index)
 
 class Expression: pass # for type hinting
 class Expression:
     expr1:Expression = None
     expr2:Expression = None
-    var:Var = None
+    var:str = ""
     is_lambda:bool = False
     is_var:bool = False
 
 
-    def __init__(self, index: int, content: str):
+    def __init__(self, content: str, index: int):
         self.index = index
         self.content = content
-        if len(content) == 0: do_err("Empty expression!",index)
+        if len(content) == 0: do_err("Empty expression",index)
         if content.count(' ') == 0:
             self.is_var = True
             check_var(content,index)
         elif content[0] == '\\':
             self.is_lambda = True
-            spl = content.split(' ')
-            if len(spl) < 2: do_err("Too little arguments",index)
-            var_str = spl[0][1:]
+            spl = content.find(' ')
+            is_bracket = False
+            if content.find('(') < spl:
+                spl = content.find('(')
+                is_bracket = True
+            var_str = content[1:spl]
+            if not is_bracket: spl += 1
             check_var(var_str,index+1)
-
-
-
-
+            self.var = var_str
+            self.expr1 = Expression(content[spl:index+len(content)])
 
 
 def remove_excess_spaces(s:str) -> str:
@@ -98,13 +95,13 @@ def bracket_split(s:str, index:int, level:int, l:list) -> int:
         if s[index] == '(':
             new_index = bracket_split(s,index + 1, level + 1, l)
             if index + 1 == new_index or s[index + 1:new_index] == ' ':
-                do_err("Brackets without content!",index)
+                do_err("Brackets without content",index)
             new_el = (level,s[index + 1:new_index],index)
             l.append(new_el)
             #if parent is not None: parent.children.append(new_el)
             index = new_index
         elif s[index] == ')':
-            if level == 0: do_err("Too many closed brackets!",index)
+            if level == 0: do_err("Too many closed brackets",index)
             return index
         index += 1
-    if level != 0: do_err("Too many open brackets!", index)
+    if level != 0: do_err("Too many open brackets", index)
