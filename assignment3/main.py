fdb4b9487e86c8fc55dea7d0cdb8a4adce682fe1
Florian <s3612759@vuw.leidenuniv.nl>
2023-12-30 23:05:26 +0100

    made assingment3 sorry i forgot to commit i was to focussed..

---
+++ assignment3/main.py
@@ -0,0 +1,19 @@
+import strhelper as sh, sys
+
+DEBUG = False
+
+if __name__ == "__main__":
+    times = 0
+    if len(sys.argv) != 2:
+        print("ERROR: No file argument or too many arguments were given", file=sys.stderr)
+        exit(1)
+    with open(sys.argv[1],"r") as f: lines = f.readlines()
+    for line in lines:
+        inp = line.replace('\n','').replace('\r','') # remove \r for windows to linux and the extra \n
+        inp = sh.remove_excess_spaces(inp) # remove spaces
+        print(inp)
+        exit()
+        expr = sh.Expression(inp)
+        print(sh.tree_to_str(expr))
+        if DEBUG:
+            sh.print_expr_tree("#",expr,False)
---
+++ assignment3/strhelper.py
@@ -0,0 +1,212 @@
+import sys
+
+# init used characters
+ALPHA = "abcdefghijklmnopqrstuvwxyz"
+NUM = "0123456789"
+ALLCHAR = ALPHA + NUM
+
+# function for handling errors
+def do_err(reason:str = "Something went wrong", index:int = -1):
+    print("ERROR: " + reason + (f" | at char: {index}" if index >= 0 else ""), file=sys.stderr)
+    exit(1)
+
+# checks if given string is a legal var
+def check_var(s:str,in_str:str = ""):
+    if len(s) == 0: do_err("Empty var")
+    if s[0] not in ALPHA: do_err("Var must start with a letter") # must start with letter
+    # check if the var only contains legal characters stop at ( or )
+    for c in s:
+        if c == '(' or c == ')': break
+        if c not in ALLCHAR: do_err("Illegal var name")
+
+# find index of closing bracket which belongs to the opening bracket at char 0
+def find_closing_bracket(s:str) -> int:
+    open_count = 0
+    for i in range(len(s)):
+        if s[i] == '(': open_count += 1
+        elif s[i] == ')':
+            open_count -= 1
+            if open_count == 0:
+                return i
+    do_err("Too many open brackets!")
+
+class Type: pass
+class Type:
+    def __init__(self, content: str = "", top:Type = None):
+        self.content = content
+        self.top = top
+        self.type:Type = None
+
+        if len(content) == 0:
+            return
+        elif content[0] == '(':
+            self.type = Type(content[1:len(content)-1],self)
+        elif '->' not in content:
+            check_var(content)
+
+
+class Expression: pass # for type hinting
+class Expression: # class used for making a binary tree of tokens, recursively
+    def __init__(self, content: str = "", top:Expression = None, is_abstract:bool = False):
+        self.content = content
+        # for expressions with only 1 child like 'expr = (expr)' only expr1 is used
+        self.expr1:Expression = None
+        self.expr2:Expression = None
+        self.top:Expression = top
+        self.type:Type = None
+        # types of expression (var also counts as expression to keep it simple)
+        self.is_lambda:bool = False
+        self.is_var:bool = False
+        self.has_lambda:bool = False
+        self.is_double:bool = False
+        self.is_judgement:bool = False
+        self.var:str = ""
+
+        if len(content) == 0: # end here if empty constructor
+            return
+        elif content[0] == '\\': # for lambda expressions
+            # find the var (spaces between \ and var have already been removed)
+            spl = content.find(' ')
+            is_bracket = False
+            # check if var and expr are separated by ( or space
+            l_bracket_index = content.find('(')
+            closing_bracket = -1
+            if l_bracket_index != -1 and (l_bracket_index < spl or spl == -1):
+                spl = l_bracket_index
+                is_bracket = True
+                closing_bracket = find_closing_bracket(content[l_bracket_index:])
+
+            self.is_lambda = True # set type
+
+            var_str = content[1:spl] # copy var out
+            if not is_bracket: spl += 1 # if the seperation is done by a space we jump it
+
+            check_var(var_str) # check if its legal
+            self.var = var_str
+            if self.content[spl] == '\\':
+                self.expr1 = Expression(content[spl:len(content)],self)
+            elif is_bracket:
+                self.expr1 = Expression(content[spl:spl+closing_bracket+1],self) # define expression via recursion
+            elif is_abstract:
+                spl2 = content[spl:].find(' ')
+                if spl2 == -1:
+                    self.expr1 = Expression(content[spl:len(content)],self)
+                else:
+                    spl2 += spl
+                    self.is_lambda = False
+                    self.is_double = True
+
+                    self.expr1 = Expression(content[0:spl2],self)
+                    self.expr2 = Expression(content[spl2+1:len(content)],self)
+            else:
+                self.expr1 = Expression(content[spl:len(content)],self)
+
+        elif content[0] == '(':
+            closing_index = find_closing_bracket(content)
+            if len(content) - 1 == closing_index: # check if there is only one (expr) and not (expr)(expr)
+                self.expr1 = Expression(content[1:closing_index],self, True) # top is bracket (for lambda abstraction)
+            elif content[closing_index+1:] == ':': ######### assign3
+                self.is_judgement = True
+            else: # else we have to cut them into 2 expressions
+                self.is_double = True
+                self.expr1 = Expression(content[0:closing_index+1],self)
+                self.expr2 = Expression(content[closing_index+1:len(content)],self)
+        elif content.count(' ') == 0 and content.count('(') == 0: # check if its truly solely a var
+            self.is_var = True
+            check_var(content)
+            self.var = content
+        else: # for 2 expressions next to each other
+            l_bracket_index = content.find('(')
+            self.is_double = True
+            if l_bracket_index != -1: # for for example: "expr(expr)"
+                self.expr1 = Expression(content[0:l_bracket_index],self)
+                self.expr2 = Expression(content[l_bracket_index:len(content)],self)
+            else: # for for example: "expr expr"
+                space_index = content.find(' ')
+                self.expr1 = Expression(content[0:space_index],self)
+                self.expr2 = Expression(content[space_index+1:len(content)],self)
+        if not self.is_var:
+            self.has_lambda = self.expr1.is_lambda
+
+def print_expr_tree(prefix:str, expr:Expression, is_left:bool): # prints binary tree using BT (for debug)
+    if expr is None: return
+    print(prefix + ("├──" if is_left else "└──") + tree_to_str(expr))
+    print_expr_tree(prefix + ("│   " if is_left else "    "), expr.expr1, True)
+    print_expr_tree(prefix + ("│   " if is_left else "    "), expr.expr2, False)
+
+# updated to remove spaces surrounding ':'
+def remove_excess_spaces(s:str) -> str:
+    # remove excess spaces
+    while s.count("  ") > 0:
+        s = s.replace("  ", " ")
+    # remove spaces after closing bracket
+    l = list(s)
+    i = 0
+    is_bracket = False
+    is_colon = False
+    while i < len(l):
+        if l[i] == ')':
+            is_bracket = True
+            is_colon = False
+        elif l[i] == ':':
+            is_colon = True
+            is_bracket = False
+        elif l[i] == ' ':
+            if is_bracket or is_colon:
+                l.pop(i)
+                continue
+        else:
+            is_bracket = False
+            is_colon = False
+        i += 1
+    # remove spaces before opening bracket
+    i = len(l) - 1
+    is_bracket = False
+    while i >= 0:
+        if l[i] == '(' or l[i] == ':':
+            is_bracket = True
+        elif l[i] == '-':
+            if l[i+1] != '>': do_err("Missing > after \''-\'")
+            is_bracket = True
+        elif l[i] == '>':
+            if l[i-1] != '-': do_err("Missing - before \''>\'")
+            is_bracket = True
+        elif l[i] == ' ':
+            if is_bracket:
+                l.pop(i)
+                continue
+        else:
+            is_bracket = False
+        i -= 1
+    i = 0
+    is_lambda = False
+    while i < len(l):
+        if l[i] == '\\':
+            is_lambda = True
+        elif l[i] == ' ':
+            if is_lambda:
+                l.pop(i)
+                continue
+        else:
+            is_lambda = False
+        i += 1
+    return "".join(l)
+
+def tree_to_str(expr:Expression, has_brackets:bool = False) -> str:
+    if expr.is_lambda:
+        return "\\" + expr.var + " " + tree_to_str(expr.expr1)
+    if expr.is_var:
+        return expr.var
+    if expr.expr2 is None:
+        if expr.top is None or has_brackets:
+            return f"{tree_to_str(expr.expr1,expr.top is None or has_brackets)}"
+        #if not has_brackets and expr.top is not None and expr.top.is_double:
+        return f"({tree_to_str(expr.expr1,True)})"
+
+    else:
+        if expr.expr1.is_var:
+            return f"{tree_to_str(expr.expr1)}({tree_to_str(expr.expr2,True)})"
+        elif expr.expr2.is_var:
+            return f"({tree_to_str(expr.expr1,True)}){tree_to_str(expr.expr2)}"
+        else:
+            return f"({tree_to_str(expr.expr1,True)})({tree_to_str(expr.expr2,True)})"
